3f41d02bc2b2aac4a58417f2d8727f4ee546c0f1
yashweblife <yashweblife20@gmail.com>
2022-10-15 10:01:29 -0500

    Added Notification Card

---
+++ src/components/NotificationCardComponent.tsx
@@ -0,0 +1,27 @@
+import { IonCard, IonCardContent, IonCardHeader, IonIcon, IonTitle, IonToolbar } from "@ionic/react"
+import { personOutline } from "ionicons/icons"
+
+interface NotificationcardDataInterface{
+    id:string,
+    priority:string,
+    sender:string,
+    message:string
+}
+
+const NotificationCardComponent:React.FC<NotificationcardDataInterface> = (item)=>{
+    return(
+        <IonCard key={`${item.id}`}>
+        <IonCardHeader>
+          <IonToolbar>
+            <IonIcon icon={personOutline} className={`${item.priority}`} slot="start"></IonIcon>
+            <IonTitle class="ion-margin-start">{item.sender}</IonTitle>
+          </IonToolbar>
+        </IonCardHeader>
+        <IonCardContent>
+          {item.message}
+        </IonCardContent>
+      </IonCard>
+    )
+}
+
+export default NotificationCardComponent
