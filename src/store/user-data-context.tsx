756654b911fb4448072dd641e8229408c94fb9c7
yashweblife <yashweblife20@gmail.com>
2022-10-15 07:21:01 -0500

    Created User data context

---
+++ src/store/user-data-context.tsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+interface UserDataContext{
+
+}
+
+
+const UserDataContext = createContext<UserDataContext>({})
+export const UserDataContextProvider = (props:any)=>{
+    const context:UserDataContext = {}
+    return(
+        <UserDataContext.Provider value={context}>{props.children}</UserDataContext.Provider>
+    )
+}
