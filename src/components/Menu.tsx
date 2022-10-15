acbab3e2077080f042ead76926dd4ea1ff2a2ac1
yashweblife <yashweblife20@gmail.com>
2022-10-15 07:11:19 -0500

    Created side menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,43 @@
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonTitle, IonToolbar } from "@ionic/react"
+import { add, addOutline, personOutline } from "ionicons/icons"
+import { generateID } from "../utils/utils"
+import "./Menu.scss"
+
+const Menu:React.FC = ()=>{
+    return(
+        <IonMenu contentId="main-content" >
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Lists</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonList>
+                    <IonItem detail={true}>
+                        <IonLabel>
+                            Contacts
+                        </IonLabel>
+                    </IonItem>
+                    <IonItem detail={true}>
+                        <IonLabel>
+                            Todos
+                        </IonLabel>
+                    </IonItem>
+                    <IonItem detail={true}>
+                        <IonLabel>
+                            Profile
+                        </IonLabel>
+                    </IonItem>
+                    <IonItem detail={true}>
+                        <IonLabel>
+                            Settings
+                        </IonLabel>
+                    </IonItem>
+
+                </IonList>
+            </IonContent>
+        </IonMenu>
+    )
+}
+
+export default Menu
